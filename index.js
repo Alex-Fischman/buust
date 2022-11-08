b7459a4fc2fd1e514f775fd18b42542918c6673d
Alex Fischman <alexdfischman@gmail.com>
2022-11-07 23:53:20 -0800

    refactor, smoothed camera motion

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ window.addEventListener("resize", () => {
 window.dispatchEvent(new Event("resize"));
 
 const CUBES_PER_SIDE = 10;
-const CUBES_DISTANCE = 10;
-scene.fog = new THREE.Fog(0x000000, 1, CUBES_PER_SIDE / 2 * CUBES_DISTANCE);
+const CUBES_DISTANCE = 5;
+scene.fog = new THREE.Fog(0x000000, 1, (CUBES_PER_SIDE / 2 - 1) * CUBES_DISTANCE);
 
 const cubes = new THREE.InstancedMesh(
 	new THREE.BoxGeometry(2, 2, 2),
@@ -45,6 +45,25 @@ document.addEventListener("mousemove", e => {
 	camera.quaternion.setFromEuler(euler);
 });
 
+const distanceToCubes = point => {
+	const matrix = new THREE.Matrix4();
+	const vector = new THREE.Vector3();
+	let distance = Infinity;
+	for (let i = 0; i < cubes.count; ++i) {
+		cubes.getMatrixAt(i, matrix);
+		vector.copy(point).applyMatrix4(matrix.invert());
+		vector.fromArray(vector.toArray().map(x => Math.abs(x) - 1));
+		const a = Math.min(0, Math.max(vector.x, vector.y, vector.z));
+		const b = vector.max(new THREE.Vector3()).length();
+		distance = Math.min(distance, a + b);
+	}
+	return distance;
+};
+
+const player = {
+	position: new THREE.Vector3(),
+};
+
 const update = dt => {
 	{
 		const position = new THREE.Vector3();
@@ -66,11 +85,9 @@ const update = dt => {
 					position.y += hash(seed + 1) * 0.8;
 					position.z += hash(seed + 2) * 0.8;
 					position.multiplyScalar(CUBES_DISTANCE);
-					
 					rotation.x = hash(seed + 3) * Math.PI * 2;
 					rotation.y = hash(seed + 4) * Math.PI * 2;
 					rotation.z = hash(seed + 5) * Math.PI * 2;
-					
 					matrix.compose(position, quaternion.setFromEuler(rotation), scale);
 					const index = i * CUBES_PER_SIDE * CUBES_PER_SIDE + j * CUBES_PER_SIDE + k;
 					cubes.setMatrixAt(index, matrix);
@@ -84,38 +101,23 @@ const update = dt => {
 	const direction = new THREE
 		.Vector3(!!keys.d - !!keys.a, !!keys.q - !!keys.e, !!keys.s - !!keys.w)
 		.applyMatrix4(new THREE.Matrix4().extractRotation(camera.matrix));
-	const position = camera.position;
 
-	const distanceToCubes = point => {
-		const matrix = new THREE.Matrix4();
-		const vector = new THREE.Vector3();
-		let distance = Infinity;
-		for (let i = 0; i < cubes.count; ++i) {
-			cubes.getMatrixAt(i, matrix);
-			vector.copy(point).applyMatrix4(matrix.invert());
-			vector.fromArray(vector.toArray().map(x => Math.abs(x) - 1));
-			const a = Math.min(0, Math.max(vector.x, vector.y, vector.z));
-			const b = vector.max(new THREE.Vector3()).length();
-			distance = Math.min(distance, a + b);
-		}
-		return distance;
-	};
-
-	const distance = distanceToCubes(position) - 0.5;
+	const distance = distanceToCubes(player.position) - 0.5;
 	const normal = new THREE.Vector3(
-		distanceToCubes(new THREE.Vector3( 0.001, 0, 0).add(position)) -
-		distanceToCubes(new THREE.Vector3(-0.001, 0, 0).add(position)),
-		distanceToCubes(new THREE.Vector3(0,  0.001, 0).add(position)) -
-		distanceToCubes(new THREE.Vector3(0, -0.001, 0).add(position)),
-		distanceToCubes(new THREE.Vector3(0, 0,  0.001).add(position)) -
-		distanceToCubes(new THREE.Vector3(0, 0, -0.001).add(position)),
+		distanceToCubes(new THREE.Vector3( 0.001, 0, 0).add(player.position)) -
+		distanceToCubes(new THREE.Vector3(-0.001, 0, 0).add(player.position)),
+		distanceToCubes(new THREE.Vector3(0,  0.001, 0).add(player.position)) -
+		distanceToCubes(new THREE.Vector3(0, -0.001, 0).add(player.position)),
+		distanceToCubes(new THREE.Vector3(0, 0,  0.001).add(player.position)) -
+		distanceToCubes(new THREE.Vector3(0, 0, -0.001).add(player.position)),
 	);
 
-	const move  = direction.clone().setLength(Math.min(movement, distance));
-	const slide = direction.clone().setLength(Math.max(movement - distance, 0));
-	if (direction.dot(normal) < 0) slide.projectOnPlane(normal);
+	const unobstructed = direction.clone().setLength(Math.min(movement, distance));
+	direction.setLength(Math.max(movement - distance, 0));
+	if (direction.dot(normal) < 0) direction.projectOnPlane(normal);
 	
-	camera.position.add(move).add(slide);
+	player.position.add(unobstructed).add(direction);
+	camera.position.lerp(player.position, 0.2);
 };
 
 let then = performance.now();
