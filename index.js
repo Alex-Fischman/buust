6c3539dd00cf1ce1fa7f4a5a5c327fdc971c7d61
Alex Fischman <alexdfischman@gmail.com>
2022-11-07 21:22:19 -0800

    collision works but is still too slow to run 1000 cubes

--- index.js
+++ index.js
@@ -7,37 +7,31 @@ window.addEventListener("resize", () => {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.render(scene, camera);
 });
 window.dispatchEvent(new Event("resize"));
-
-// Preallocated for use in computations
-const _vector = new THREE.Vector3();
-const _euler = new THREE.Euler();
-const _matrix = new THREE.Matrix4();
-const _quaternion = new THREE.Quaternion();
 
 const cubes = new THREE.InstancedMesh(
 	new THREE.BoxGeometry(2, 2, 2),
 	new THREE.MeshToonMaterial(),
-	1e4,
+	2e4,
 );
-const scale = new THREE.Vector3(1, 1, 1);
-for (let i = 0; i < cubes.count; ++i) {
-	_vector.x = (Math.random() * 2 - 1) * Math.sqrt(cubes.count);
-	_vector.y = (Math.random() * 2 - 1) * Math.sqrt(cubes.count);
-	_vector.z = (Math.random() * 2 - 1) * Math.sqrt(cubes.count);
-	_euler.x = Math.random() * Math.PI * 2;
-	_euler.y = Math.random() * Math.PI * 2;
-	_euler.z = Math.random() * Math.PI * 2;
-	_quaternion.setFromEuler(_euler);
-	_matrix.compose(_vector, _quaternion, scale);
-	cubes.setMatrixAt(i, _matrix);
+{
+	const position = new THREE.Vector3();
+	const rotation = new THREE.Euler();
+	const quaternion = new THREE.Quaternion();
+	const matrix = new THREE.Matrix4();
+	const scale = new THREE.Vector3(1, 1, 1);
+	for (let i = 0; i < cubes.count; ++i) {
+		position.x = (Math.random() * 2 - 1) * Math.sqrt(cubes.count);
+		position.y = (Math.random() * 2 - 1) * Math.sqrt(cubes.count);
+		position.z = (Math.random() * 2 - 1) * Math.sqrt(cubes.count);
+		rotation.x = Math.random() * Math.PI * 2;
+		rotation.y = Math.random() * Math.PI * 2;
+		rotation.z = Math.random() * Math.PI * 2;
+		cubes.setMatrixAt(i, matrix.compose(position, quaternion.setFromEuler(rotation), scale));
+	}
 }
-_vector.set(0, -2.5, 0);
-_euler.set(0, 0, 0);
-_quaternion.setFromEuler(_euler);
-_matrix.compose(_vector, _quaternion, scale);
-cubes.setMatrixAt(0, _matrix);
 cubes.geometry.normalizeNormals();
 scene.add(cubes);
 
@@ -60,37 +54,58 @@ document.addEventListener("keydown", e => keys[event.key] = true);
 document.addEventListener("keyup", e => keys[event.key] = false);
 document.addEventListener("mousemove", e => {
 	if (!document.pointerLockElement) return;
-	_euler.reorder("YXZ");
-	_euler.setFromQuaternion(camera.quaternion);
-	_euler.y -= event.movementX * 0.002;
-	_euler.x -= event.movementY * 0.002;
-	_euler.x = Math.max(-Math.PI / 2, Math.min(Math.PI / 2, _euler.x));
-	camera.quaternion.setFromEuler(_euler);
-	_euler.reorder("XYZ");
+	const euler = new THREE.Euler(0, 0, 0, "YXZ").setFromQuaternion(camera.quaternion);
+	euler.y -= event.movementX * 0.002;
+	euler.x -= event.movementY * 0.002;
+	euler.x = Math.max(-Math.PI / 2, Math.min(Math.PI / 2, euler.x));
+	camera.quaternion.setFromEuler(euler);
 });
 
-const signedDistance = point => {
-	let distance = Infinity;
-	for (let i = 0; i < cubes.count; ++i) {
-		cubes.getMatrixAt(i, _matrix);
-		_vector.copy(point).applyMatrix4(_matrix.invert());
-		_vector.fromArray(_vector.toArray().map(x => Math.abs(x) - 1));
-		const a = Math.min(0, Math.max(_vector.x, _vector.y, _vector.z));
-		const b = _vector.max(new THREE.Vector3()).length();
-		distance = Math.min(distance, a + b);
-	}
-	return distance;
-};
 const update = dt => {
-	const head = signedDistance(new THREE.Vector3(0, 0, 0).add(camera.position)) - 0.5;
-	const body = signedDistance(new THREE.Vector3(0, -0.5, 0).add(camera.position)) - 0.5;
-	const foot = signedDistance(new THREE.Vector3(0, -1, 0).add(camera.position)) - 0.5;
-	const travel = Math.min(head, body, foot);
+	const movement = dt * 10;
+	const direction = new THREE
+		.Vector3(!!keys.d - !!keys.a, !!keys.q - !!keys.e, !!keys.s - !!keys.w)
+		.applyMatrix4(new THREE.Matrix4().extractRotation(camera.matrix));
+	const position = camera.position;
 
-	_vector.set(!!keys.d - !!keys.a, !!keys.q - !!keys.e, !!keys.s - !!keys.w).setLength(dt * 10);
-	_vector.applyMatrix4(_matrix.extractRotation(camera.matrix));
-	// todo: collision resolution
-	camera.position.add(_vector);
+	const matrix = new THREE.Matrix4();
+	const inverted = [...Array(cubes.count)].map((_, i) => {
+		cubes.getMatrixAt(i, matrix);
+		return matrix.clone().invert();
+	});
+	const vector = new THREE.Vector3();
+	const distances = [
+		new THREE.Vector3(),
+		new THREE.Vector3( 0.001, 0, 0),
+		new THREE.Vector3(-0.001, 0, 0),
+		new THREE.Vector3(0,  0.001, 0),
+		new THREE.Vector3(0, -0.001, 0),
+		new THREE.Vector3(0, 0,  0.001),
+		new THREE.Vector3(0, 0, -0.001),
+	].map(offset => offset.add(position)).map(point => {
+		let distance = Infinity;
+		for (let i = 0; i < cubes.count; ++i) {
+			vector.copy(point).applyMatrix4(inverted[i]);
+			vector.fromArray(vector.toArray().map(x => Math.abs(x) - 1));
+			const a = Math.min(0, Math.max(vector.x, vector.y, vector.z));
+			const b = vector.max(new THREE.Vector3()).length();
+			distance = Math.min(distance, a + b);
+		}
+		return distance;
+	});
+
+	const distance = distances[0] - 0.5;
+	const normal = new THREE.Vector3(
+		distances[1] - distances[2],
+		distances[3] - distances[4],
+		distances[5] - distances[6],
+	);
+
+	const move  = direction.clone().setLength(Math.min(movement, distance));
+	const slide = direction.clone().setLength(Math.max(movement - distance, 0));
+	if (direction.dot(normal) < 0) slide.projectOnPlane(normal);
+	
+	camera.position.add(move).add(slide);
 };
 
 let then = performance.now();
